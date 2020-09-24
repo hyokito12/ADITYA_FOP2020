ffb19624ca37767f2ff50aee2f7d8e8851b059f8
repl.it user <replituser@example.com>
2020-09-24 09:26:21 +0000

    Change main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,6 @@
 using namespace std;
 
 int main (){
-  int agethisyear,agenextyear,agelastyear,i = 1;
-
-  //Input your age
-  cout<<"Input your age this year : ";
-  cin>>agethisyear;
-
-  //Formula of The age
-  agenextyear = agethisyear + (i++);
-  agelastyear = agethisyear - (--i);
-
-  //Compute your age
-  cout<<"Your age next year : ";
-  cout<<agenextyear<<endl;
-
-  cout<<"Your age last year :";
-  cout<<agelastyear<<endl;
+  cout << "Welcome to FOP LAB!" << endl;
   return 0;
 }
