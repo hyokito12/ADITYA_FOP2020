9c959ee33b6c72ac6b556d2c9f37f5c9de214680
Aditya Sanjaya <adityasanjaya24@gmail.com>
2020-10-02 10:19:31 +0800

    Test Push pake VSCode

--- LAB03/question1.cpp
+++ LAB03/question1.cpp
@@ -1,18 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
   // Variable declaration
   int num;
 
   // User input
-  cout  << "Give me a number!\n";
-  cin   >> num;
-  
+  cout << "Give me a number!\n";
+  cin >> num;
+
   // Check num and display output
-  if (num % 2 == 0){
+  if (num % 2 == 0)
+  {
     cout << "Your number is even!\n";
-  } else {
+  }
+  else
+  {
     cout << "Your number is odd!\n";
   }
 }
